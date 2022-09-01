3ce596fcfd793c19249be76c07a93b7b77feaeec
Duedot43 <gendillj@gmail.com>
2022-09-01 10:44:35 -0600

    END ME

--- C++/buzzer/buzzer.ino
+++ C++/buzzer/buzzer.ino
@@ -1,12 +1,106 @@
-void setup() {
+#include "notes.h"
+
+void playNote(int note, int duration)
+// This custom function takes two parameters, note and duration to make playing songs easier.
+// Each of the notes have been #defined in the notes.h file. The notes are broken down by
+// octave and sharp (s) / flat (b).
+{
+    tone(9, note, duration);
+    delay(duration);
+}
+
+void setup()
+{
     pinMode(9, OUTPUT);
     pinMode(12, INPUT_PULLUP);
 }
 
-void loop() {
-    if (digitalRead(12) == LOW) {
-        tone(9, 1000);
-        delay(125);
-        noTone(9);
+void loop()
+{
+    if (digitalRead(12) == LOW)
+    {
+        playNote(noteB5, SN);
+        playNote(noteB5, EN);
+        playNote(noteG4, EN);
+        playNote(noteD5, EN);
+        playNote(noteB4, QN);
+        playNote(noteE5, QN);
+        playNote(noteB4, QN);
+        playNote(noteE5, QN);
+        playNote(noteA4, QN);
+        playNote(noteE5, QN);
+        playNote(noteA4, QN);
+        playNote(noteE5, QN);
+        playNote(noteG4, QN);
+        playNote(noteE5, QN);
+        playNote(noteG4, QN);
+        playNote(noteE5, QN);
+        playNote(noteG4, QN);
+        playNote(noteE5, QN);
+        playNote(noteG4, QN);
+        playNote(noteE5, QN);
+        playNote(noteF4, QN);
+        playNote(noteE5, QN);
+        playNote(noteC4, QN);
+        playNote(noteE5, QN);
+        playNote(noteD4, QN);
+        playNote(noteD5, QN);
+        playNote(noteE4, QN);
+        playNote(noteE5, QN);
+        playNote(noteF4, QN);
+        playNote(noteG5, QN);
+        playNote(noteA3, QN);
+        playNote(noteD5, QN);
+        playNote(noteE4, QN);
+        playNote(noteD5, QN);
+        playNote(noteE5, QN);
+        playNote(noteD4, QN);
+        playNote(noteA3, QN);
+        playNote(Rest, QN);
+        playNote(noteA3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE5, QN);
+        playNote(noteA3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE3, QN);
+        playNote(noteA3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE3, QN);
+        playNote(noteE5, QN);
+        playNote(noteB3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE4, QN);
+        playNote(noteE5, QN);
+        playNote(noteB3, QN);
+        playNote(noteE3, QN);
+        playNote(noteE4, QN);
+        playNote(noteE5, QN);
+        playNote(noteB3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE5, QN);
+        playNote(noteC5, EN);
+        playNote(noteE5, QN);
+        playNote(noteC5, QN);
+        playNote(noteB3, QN);
+        playNote(Rest, EN);
+        playNote(noteC4, EN);
+        playNote(noteE3, QN);
+        playNote(noteE4, QN);
+        playNote(noteA3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE5, QN);
+        playNote(noteA3, QN);
+        playNote(noteE5, QN);
+        playNote(noteE5, QN);
+        playNote(noteE3, QN);
+        playNote(noteE5, QN);
+        //END ME
     }
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,13 +1,107 @@
 # 1 "/home/syntax/Programs/ArduinoPrograms/C++/buzzer/buzzer.ino"
-void setup() {
+# 2 "/home/syntax/Programs/ArduinoPrograms/C++/buzzer/buzzer.ino" 2
+
+void playNote(int note, int duration)
+// This custom function takes two parameters, note and duration to make playing songs easier.
+// Each of the notes have been #defined in the notes.h file. The notes are broken down by
+// octave and sharp (s) / flat (b).
+{
+    tone(9, note, duration);
+    delay(duration);
+}
+
+void setup()
+{
     pinMode(9, 0x1);
     pinMode(12, 0x2);
 }
 
-void loop() {
-    if (digitalRead(12) == 0x0) {
-        tone(9, 1000);
-        delay(125);
-        noTone(9);
+void loop()
+{
+    if (digitalRead(12) == 0x0)
+    {
+        playNote(988, 200 /* # of milliseconds per beat*//4 /* ...and a sixteenth note.*/);
+        playNote(988, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(392, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(587, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(494, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(494, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(440, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(440, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(392, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(392, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(392, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(392, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(349, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(262, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(294, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(587, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(330, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(349, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(784, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(587, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(330, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(587, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(294, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(0, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(247, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(330, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(247, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(330, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(247, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(523, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(523, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(247, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(0, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(262, 200 /* # of milliseconds per beat*//2 /* ...an eighth note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(330, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(220, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(165, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        playNote(659, 200 /* # of milliseconds per beat*/ /* ...a quarter note...*/);
+        //END ME
     }
 }
