a1b8b49e6dd0d96fc3691d462cfc5da08d5eceea
Duedot43 <gendillj@gmail.com>
2022-09-08 08:25:21 -0600

    Forgor to upload this

--- C++/driveForward/calibrate.ino
+++ C++/driveForward/calibrate.ino
@@ -1,19 +1,18 @@
-# 1 "/home/syntax/Programs/ArduinoPrograms/C++/driveForward/calibrate.ino"
 /**
  * @brief Calibrate moder to drive straight
  * 
  * @file calibrate.ino
  * @date 2020-04-20
  */
-# 8 "/home/syntax/Programs/ArduinoPrograms/C++/driveForward/calibrate.ino" 2
+#include <RedBot.h>
 RedBotMotors motors;
 
 
 void setup() {
-    pinMode(12, 0x2);
+    pinMode(12, INPUT_PULLUP);
 }
 void loop() {
-    if (digitalRead(12) == 0x0) {
+    if (digitalRead(12) == LOW) {
         motors.rightMotor(190);
         motors.leftMotor(200);
         delay(8000);
