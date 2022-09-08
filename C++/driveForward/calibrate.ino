996051c3a8bbac6e70b45ef6318581ea59910bc9
Duedot43 <gendillj@gmail.com>
2022-09-08 14:42:27 -0600

    Inches

--- C++/driveForward/calibrate.ino
+++ C++/driveForward/calibrate.ino
@@ -1,23 +1,28 @@
 /**
  * @brief Calibrate moder to drive straight
- * 
+ *
  * @file calibrate.ino
  * @date 2020-04-20
  */
 #include <RedBot.h>
 RedBotMotors motors;
 
-
-void setup() {
+void forwardInch(int inch)
+{
+    motors.rightMotor(200);
+    motors.leftMotor(189);
+    delay((460/12) * inch);
+    motors.stop();
+    motors.brake();
+}
+void setup()
+{
     pinMode(12, INPUT_PULLUP);
 }
-void loop() {
-    if (digitalRead(12) == LOW) {
-        delay(300);
-        motors.rightMotor(200);
-        motors.leftMotor(189);
-        delay(460);
-        motors.stop();
-        motors.brake();
+void loop()
+{
+    if (digitalRead(12) == LOW)
+    {
+        forwardInch(12);
     }
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,24 +1,29 @@
 # 1 "/home/syntax/Programs/ArduinoPrograms/C++/driveForward/calibrate.ino"
 /**
  * @brief Calibrate moder to drive straight
- * 
+ *
  * @file calibrate.ino
  * @date 2020-04-20
  */
 # 8 "/home/syntax/Programs/ArduinoPrograms/C++/driveForward/calibrate.ino" 2
 RedBotMotors motors;
 
-
-void setup() {
+void forwardInch(int inch)
+{
+    motors.rightMotor(200);
+    motors.leftMotor(189);
+    delay((460/12) * inch);
+    motors.stop();
+    motors.brake();
+}
+void setup()
+{
     pinMode(12, 0x2);
 }
-void loop() {
-    if (digitalRead(12) == 0x0) {
-        delay(300);
-        motors.rightMotor(200);
-        motors.leftMotor(189);
-        delay(460);
-        motors.stop();
-        motors.brake();
+void loop()
+{
+    if (digitalRead(12) == 0x0)
+    {
+        forwardInch(12);
     }
 }
