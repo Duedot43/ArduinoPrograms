862f1ffc54f29be729a75e0462afc0e94cd54e65
Duedot43 <gendillj@gmail.com>
2022-09-20 15:13:18 -0600

    Maze

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,15 +1,40 @@
-# 1 "/home/syntax/Programs/ArduinoPrograms/C++/8/8.ino"
-# 2 "/home/syntax/Programs/ArduinoPrograms/C++/8/8.ino" 2
+# 1 "/home/syntax/Programs/ArduinoPrograms/C++/maze/maze.ino"
+# 2 "/home/syntax/Programs/ArduinoPrograms/C++/maze/maze.ino" 2
 RedBotMotors motors;
 
-RedBotEncoder encoder = RedBotEncoder(A2, 10);
-int buttonPin = 12;
+RedBotEncoder encoder = RedBotEncoder(A2, A5);
+
 int countsPerRev = 192; // 4 pairs of N-S x 48:1 gearbox = 192 ticks per wheel rev
 
 float wheelDiam = 2.56; // diam = 65mm / 25.4 mm/in
 float wheelCirc = 3.1415926535897932384626433832795 * wheelDiam; // Redbot wheel circumference = pi*D
+const int buttonPin = 12;
 
-void driveForward(float distance, int motorPower)
+void setup()
+{
+    pinMode(buttonPin, 0x2);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+
+    if (digitalRead(buttonPin) == 0x0)
+    {
+        delay(500); // short delay to allow you to get your hands away before starting
+        /*Here is where you put your code to drive your robot.
+        You will be using the driveStraight, turnRight, and turnLeft functions
+        for example:*/
+        driveStraight(100, 100);
+        turnRight(87);
+        driveStraight(30, 100);
+        turnLeft(45);
+        driveStraight(20, 100);
+        motors.stop();
+    }
+}
+
+void driveStraight(float distance, int motorPower)
 {
     long lCount = 0;
     long rCount = 0;
@@ -26,7 +51,7 @@ void driveForward(float distance, int motorPower)
     int rightPower = motorPower;
 
     // variable used to offset motor power on right vs left to keep straight.
-    int offset = 2; // offset amount to compensate Right vs. Left drive
+    int offset = 3; // offset amount to compensate Right vs. Left drive
 
     numRev = distance / wheelCirc; // calculate the target # of rotations
     targetCount = numRev * countsPerRev; // calculate the target count
@@ -91,26 +116,23 @@ void driveForward(float distance, int motorPower)
         }
         delay(50); // short delay to give motors a chance to respond.
     }
+    // now apply "brakes" to stop the motors.
+    motors.brake();
 }
-
-
-
-void setup() {
-    Serial.begin(9600);
-    pinMode(buttonPin, 0x2);
+void turnRight(float angle)
+{
+    /*This is for a pivot turn.  you will need to set this function up.
+    Calibrate it to turn to 90 degrees */
+    motors.rightDrive(-100);
+    motors.leftDrive(100);
+    // my delay is 200 ms to turn 90 degrees
+    delay((angle / 90.00) * 375.00);
 }
-
-void loop() {
-    if (digitalRead(buttonPin) == 0x0)
-    {
-        driveForward(30, 200);
-        motors.leftMotor(-90);
-        delay(1150);
-        motors.stop();
-        driveForward(55, 200);
-        motors.rightMotor(90);
-        delay(1100);
-        motors.stop();
-        driveForward(25, 200);
-    }
+void turnLeft(float angle)
+{
+    // Do the same as above, but reverse the motor signs
+    motors.rightDrive(100);
+    motors.leftDrive(-100);
+    // my delay is 200 ms to turn 90 degrees
+    delay((angle / 90.00) * 375.00);
 }
