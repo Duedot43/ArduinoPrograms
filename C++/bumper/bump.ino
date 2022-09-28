634a17153862ad179d34ef5bf8ce64f0977708f5
Duedot43 <gendillj@gmail.com>
2022-09-28 15:30:53 -0600

    more

--- C++/bumper/bump.ino
+++ C++/bumper/bump.ino
@@ -1,16 +1,15 @@
-# 1 "/home/syntax/Programs/ArduinoPrograms/C++/bumper/bump.ino"
 
 
-# 4 "/home/syntax/Programs/ArduinoPrograms/C++/bumper/bump.ino" 2
+#include <RedBot.h>
 RedBotMotors motors;
 
-RedBotBumper lBumper = RedBotBumper(3); // initialzes bumper object on pin 3
+RedBotBumper lBumper = RedBotBumper(3);  // initialzes bumper object on pin 3
 RedBotBumper rBumper = RedBotBumper(11); // initialzes bumper object on pin 11
 
 int buttonPin = 12; // variable to store the button Pin 
 
-int lBumperState; // state variable to store the bumper value
-int rBumperState; // state variable to store the bumper value
+int lBumperState;  // state variable to store the bumper value
+int rBumperState;  // state variable to store the bumper value
 
 void setup()
 {
@@ -21,23 +20,23 @@ void setup()
 void loop()
 {
   forward(255);
-  lBumperState = lBumper.read(); // default INPUT state is HIGH, it is LOW when bumped
-  rBumperState = rBumper.read(); // default INPUT state is HIGH, it is LOW when bumped
+  lBumperState = lBumper.read();  // default INPUT state is HIGH, it is LOW when bumped
+  rBumperState = rBumper.read();  // default INPUT state is HIGH, it is LOW when bumped
   Serial.print("Left     Right");
   Serial.print(lBumperState);
   Serial.print("   ");
   Serial.println(rBumperState);
 
-    if (lBumperState == 0x0) // left side is bumped/
-  {
-    reverse(); // backs up
-    turnRight(); // turns
+    if (lBumperState == LOW) // left side is bumped/
+  { 
+    reverse();    // backs up
+    turnRight();  // turns
   }
 
-  if (rBumperState == 0x0) // right side is bumped/
-  {
-    reverse(); // backs up
-    turnLeft(); // turns
+  if (rBumperState == LOW) // right side is bumped/
+  { 
+    reverse();   // backs up
+    turnLeft();  // turns
   }
 
 }
@@ -55,25 +54,25 @@ void reverse()
   motors.drive(-255);
   delay(500);
   motors.brake();
-  delay(100); // short delay to let robot fully stop
+  delay(100);  // short delay to let robot fully stop
 }
 
 // turnRight() function -- turns RedBot to the Right
 void turnRight()
 {
-  motors.leftMotor(-150); // spin CCW
+  motors.leftMotor(-150);  // spin CCW
   motors.rightMotor(-150); // spin CCW
   delay(500);
   motors.brake();
-  delay(100); // short delay to let robot fully stop
+  delay(100);  // short delay to let robot fully stop
 }
 
 // turnRight() function -- turns RedBot to the Left
 void turnLeft()
 {
-  motors.leftMotor(+150); // spin CW
+  motors.leftMotor(+150);  // spin CW
   motors.rightMotor(+150); // spin CW
   delay(500);
   motors.brake();
-  delay(100); // short delay to let robot fully stop
+  delay(100);  // short delay to let robot fully stop
 }
