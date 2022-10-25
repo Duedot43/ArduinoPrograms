5fdba70f2468d66300d3c9deee207fd87bcc3167
Duedot43 <gendillj@gmail.com>
2022-10-25 08:21:58 -0600

    I made it do a thing

---
+++ C++/ultra/sonic/drive.ino
@@ -0,0 +1,24 @@
+void driveVar(int varPin) {
+    int driveSpeed = map(analogRead(varPin), 0, 1023, 0, 255);
+    motors.leftMotor(driveSpeed);
+    motors.rightMotor(driveSpeed);
+}
+
+void turnRight(int varPin)
+{
+  motors.leftMotor(-150);  // spin CCW
+  motors.rightMotor(155); // spin CCW
+  delay(analogRead(varPin));
+  motors.brake();
+  delay(100);  // short delay to let robot fully stop
+}
+
+// turnRight() function -- turns RedBot to the Left
+void turnLeft(int varPin)
+{
+    motors.leftMotor(150);  // spin CW
+    motors.rightMotor(-155); // spin CW
+    delay(analogRead(varPin));
+    motors.brake();
+    delay(100);  // short delay to let robot fully stop
+}
--- C++/ultra/sonic/sonic.ino
+++ C++/ultra/sonic/sonic.ino
@@ -14,14 +14,14 @@ void setup() {
 }
 
 void loop() {
-  motors.leftMotor(analogRead(A6));
-  motors.rightMotor(analogRead(A6));
-  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  driveVar(A6);
+  delay(50);  
+  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   unsigned int uSL = sonarL.ping();
   unsigned int uSR = sonarR.ping();
   
   int lDistance = sonarL.convert_cm(uSL);
-  int rDistance = sonarR.convert_cm(uSR); 
+  int rDistance = sonarR.convert_cm(uSR);
   // Send ping, get ping time in microseconds (uS).
   Serial.print("Left Side: ");
   Serial.print(lDistance); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
@@ -31,39 +31,19 @@ void loop() {
   Serial.println( rDistance);
 
 
-if( lDistance <= 20   &&  lDistance > 0 )
+if( lDistance <= map(analogRead(A4), 0, 1023, 0, 300)   &&  lDistance > 0 )
 {
   motors.stop();
-  turnLeft();
+  turnRight(A7);
 //Stop and turn right
 }
 
-if( rDistance <= 20  &&  rDistance > 0 )
+if( rDistance <= map(analogRead(A4), 0, 1023, 0, 300)  &&  rDistance > 0 )
 {
   motors.stop();
-  turnRight();
+  turnLeft(A7);
 //Stop and turn left
 }
 
 
-}
-
-  
-void turnRight()
-{
-  motors.leftMotor(-150);  // spin CCW
-  motors.rightMotor(155); // spin CCW
-  delay(analogRead(A7));
-  motors.brake();
-  delay(100);  // short delay to let robot fully stop
-}
-
-// turnRight() function -- turns RedBot to the Left
-void turnLeft()
-{
-  motors.leftMotor(150);  // spin CW
-  motors.rightMotor(-155); // spin CW
-  delay(analogRead(A7));
-  motors.brake();
-  delay(100);  // short delay to let robot fully stop
 }
